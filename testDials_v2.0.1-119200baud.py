11f23d16d1b955d60d27fddce617066ff8a8612f
Mark Janssen <dreamingforward@gmail.com>
2019-10-28 20:14:08 -0500

    minor changes and questions...

--- testDials_v2.0.1-119200baud.py
+++ testDials_v2.0.1-119200baud.py
@@ -3,20 +3,22 @@ import sys, traceback
 
 from time import sleep
 
+INTERFACE = '/dev/cu.PL2303-0030134' #!!!change this to your personal device
+
 dialsPosition=[-1,-1,-1,-1,-1,-1,-1,-1]
 dialsCounter=[0,0,0,0,0,0,0,0]
 dialDirection=[0,0,0,0,0,0,0,0]
 
 print( "IBM Python dials 6094-010 V1.0.0" )
 
-dials = serial.Serial('/dev/cu.PL2303-0030134', 119200, timeout=1, parity=serial.PARITY_ODD, bytesize=8)
+dials = serial.Serial(INTERFACE, 119200, timeout=1, parity=serial.PARITY_ODD, bytesize=8)
 
 dials.flushInput()
 dials.flush()
 
 #check to see if dials are connected:
-dials.write(chr(0x06))
-status = dials.read(1)
+dials.write(chr(0x06))  #??Probably a bit-signified byte
+status = dials.read(1)  #also:  potential race condition here:  will the output of the D&B be ready by the time this line is executed?
 
 if status:
   if status == chr(0x08):
@@ -24,7 +26,7 @@ if status:
     print ("status: ", status)
   else:
     print "Unexpected status byte:", ord(status)
-if not status:
+else:
   print( "FAIL: dials not found, NULL" )
   print( "Exiting IBM Python dials program!" )
   dials.close()
@@ -41,7 +43,10 @@ dials.write( chr(0x08) )
 # set dial precision: 0xC8 [byte]
 # set dial precision to 8 bits per dial
 dials.write(chr(0xC8))
-dials.write(chr(0xFF))
+dials.write(chr(0xFF)) 
+#the above code looks suspicous, need to understand whats going on:
+# Do the D&B use sequential writes in some predetermined order or 
+# is it using a bit-wise single byte for writing?
 
 print "Dials are ready to send data!"
 print "Turn dial to display value:"
@@ -62,7 +67,7 @@ try:
       L_byte = ord(value[1])
       HL_word = (H_byte << 8) + L_byte
       dialNo = (H_byte >> 3) & 0x07
-      position = ((H_byte << 7) & 0xFF) | (L_byte & 127)
+      position = ((H_byte << 7) & 0xFF) | (L_byte & 0x7F)
       direction= (H_byte >> 2) & 0x01
 
       # Increment or decrement counter:
@@ -126,6 +131,6 @@ try:
       #print "HL_word: ", bin(HL_word) #BINARY output of H_byte + L_byte
 except KeyboardInterrupt:
   print("\033[8B")
-  print "Control-C received IBM dials terminating!"
+  print "Control-C received.  IBM dials terminating!"
   dials.close()
   pass
